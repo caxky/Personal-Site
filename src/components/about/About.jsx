12d0d35a81d5fbc203baedb379c700b620c1c494
caxky <k_james7@outlook.com>
2022-05-17 22:27:41 -0400

    Added more content to about section and header

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,17 @@ export default function About() {
         <h2>Software Engineering, Entrepreneurship, Finance</h2>
       </div>
 
-      <div className="about"></div>
+      <div className="about">
+        <h3>{'<About/>'}</h3>
+
+        <p>I’m a third year student at the University of Ottawa studying Computer Science.</p>
+        <p>
+          My current interests are <span>financial technologies</span>, <span>blockchain and web3</span>, <span>data analysis</span>, and <span>application development</span>. 
+          I continue to explore other areas of computer science/software engineering such as <span>artificial intelligence</span>, <span>image processing</span>, etc.
+        </p>
+        <br/>
+        <p>A few technologies I’ve been working with recently:</p>
+      </div>
     </div>
   )
 }
