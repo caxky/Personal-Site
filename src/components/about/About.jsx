9121170dc163f1a4f87759a22b30d023e0f2b596
caxky <k_james7@outlook.com>
2022-05-18 22:21:14 -0400

    Center content and add padding

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,9 +4,11 @@ export default function About() {
   return (
     <div id="about">
       <div className="intro">
-        <h6>Hi, my name is</h6>
-        <h1>Kyle James.</h1>
-        <h2>Software Engineering, Entrepreneurship, Finance</h2>
+        <div className="wrapper">
+          <h6>Hi, my name is</h6>
+          <h1>Kyle James.</h1>
+          <h2>Software Engineering, Entrepreneurship, Finance</h2>
+        </div>
       </div>
 
       <div className="about">
