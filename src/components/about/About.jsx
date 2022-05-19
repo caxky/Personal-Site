9ba3360d7a670c87ab099462814910db4ef75ea1
caxky <k_james7@outlook.com>
2022-05-18 22:45:10 -0400

    Add recent technologies section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -21,6 +21,33 @@ export default function About() {
         </p>
         <br/>
         <p>A few technologies I’ve been working with recently:</p>
+
+        <div className="container">
+          <div className="item">
+            <h4>Javascript</h4>
+            <img src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" alt="" />
+          </div>
+
+          <div className="item">
+            <h4>Vue.js</h4>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png" alt="" />
+          </div>
+
+          <div className="item">
+            <h4>React</h4>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="" />
+          </div>
+
+          <div className="item">
+            <h4>Python</h4>
+            <img src="https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png" alt="" />
+          </div>
+
+          <div className="item">
+            <h4>MongoDB</h4>
+            <img src="https://cdn.iconscout.com/icon/free/png-256/mongodb-4-1175139.png" alt="" />
+          </div>
+        </div>
       </div>
     </div>
   )
