031f81f1286177d4cd5f3f89bd6b70356e31a261
caxky <k_james7@outlook.com>
2024-01-17 20:57:07 -0500

    Make navbar collapse on mobile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,81 @@
+import React, { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Button, Drawer, List, ListItem, ListItemText, useMediaQuery } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import './navbar.scss';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#112240', // $dark-blue
+    },
+    secondary: {
+      main: '#0A192F', // $darker-blue
+    },
+    background: {
+      default: '#E5E5E5', // $bone-white
+    },
+    text: {
+      primary: '#A8B2D1', // $slate
+      secondary: '#8892B0', // $dark-slate
+    },
+    aquamarine: {
+      main: '#64FFDA', // $aquamarine
+    },
+  },
+});
 
 export default function Navbar() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const isMobile = useMediaQuery('(max-width:600px)');
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
     <div className='navbar'>
-      <div className="wrapper">
-        <div className='left'>
-          <a href="#about" className='logo'>Kyle</a>
-        </div>
-        <div className='right'>
-          <a href="#about">{'<About/>'}</a>
-          <a href="#projects">{'<Projects/>'}</a>
-          <a href="#contact">{'<Contact/>'}</a>
-        </div>
-      </div>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static" color="secondary">
+          <Toolbar>
+            {isMobile ? (
+              <>
+                <Typography variant="h6" style={{ flexGrow: 1 }}>
+                  Kyle
+                </Typography>
+                <div className='icon-container'>
+                  <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
+                    <MenuIcon />
+                  </IconButton>
+                </div>
+              </>
+            ) : (
+              <>
+                <Typography variant="h6" style={{ flexGrow: 1 }}>
+                  Kyle
+                </Typography>
+                <Button color="inherit">{'<About/>'}</Button>
+                <Button color="inherit">{'<Projects/>'}</Button>
+                <Button color="inherit">{'<Contact/>'}</Button>
+              </>
+            )}
+          </Toolbar>
+        </AppBar>
+      
+        <Drawer anchor="right" open={isDrawerOpen} onClose={toggleDrawer}>
+          <List>
+            <ListItem>
+              <ListItemText primary={'<About/>'} />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary={'<Projects/>'} />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary={'<Contact/>'} />
+            </ListItem>
+          </List>
+        </Drawer>
+      </ThemeProvider>
     </div>
-  )
+  );
 }
