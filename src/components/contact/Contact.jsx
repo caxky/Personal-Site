ab55fac3a149cfa40fd25e90552aa8f50874cb90
caxky <k_james7@outlook.com>
2024-01-21 20:07:32 -0500

    include github and linkedin in contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,6 +5,25 @@ import { ThemeProvider, styled } from '@mui/material/styles';
 import { theme } from '../../assets/styles/theme';
 import './contact.scss';
 
+const ContactCard = ({title, body, link}) => {
+  return (
+    <Grid xs={12} sm={6} md={4} style={{ cursor: 'pointer' }}>
+      <a href={link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+        <CustomCard>
+          <CardContent>
+            <Typography variant="h5" component="div" color="textPrimary">
+              {title}
+            </Typography>
+            <Typography variant="body1" color="textSecondary">
+              {body}
+            </Typography>
+          </CardContent>
+        </CustomCard>
+      </a>
+    </Grid>
+  )
+}
+
 const Contact = () => {
   return (
     <ThemeProvider theme={theme}>
@@ -12,18 +31,9 @@ const Contact = () => {
         <div className="wrapper">
           <h2>{'<Contact/>'}</h2>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6} md={4}>
-              <CustomCard>
-                <CardContent>
-                  <Typography variant="h5" component="div" color="textPrimary">
-                    Email
-                  </Typography>
-                  <Typography variant="body1" color="textSecondary">
-                    mail@kylejames.me
-                  </Typography>
-                </CardContent>
-              </CustomCard>
-            </Grid>
+            <ContactCard title="Email" body="mail@kylejames.me" link="mailto:mail@kylejames.me" />
+            <ContactCard title="GitHub" body="caxky" link="https://github.com/caxky" />
+            <ContactCard title="LinkedIn" body="Kyle James" link="https://www.linkedin.com/in/kyle-james-uo/" />
           </Grid>
         </div>
       </div>
@@ -35,6 +45,7 @@ const Contact = () => {
 const CustomCard = styled(Card)({
   backgroundColor: theme.palette.secondary.main,
   color: theme.palette.text.primary,
+  margin: '5px',
   '&:hover': {
     backgroundColor: theme.palette.secondary.dark,
   },
